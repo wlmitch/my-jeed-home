619f0e6b93fc8586ea6c1b9cdf4ea0e381d3ab66
WLMitch <wlmitch@gmail.com>
2018-07-29 17:22:51 +0200

    Change script install

--- core/class/mjh.class.php
+++ core/class/mjh.class.php
@@ -31,6 +31,7 @@ class mjh extends eqLogic {
   }
 
 	public static function dependancy_install() {
+    log::remove(__CLASS__ . '_dep');
 		return array(
       'script' => dirname(__FILE__) . '/../../resources/install.sh',
       'log' => log::getPathToLog(__CLASS__ . '_dep')
