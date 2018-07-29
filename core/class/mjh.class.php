ba2f28e2da28344952b523593e4a763f8cac5048
WLMitch <wlmitch@gmail.com>
2018-07-29 16:37:09 +0200

    Test logging

--- core/class/mjh.class.php
+++ core/class/mjh.class.php
@@ -24,7 +24,7 @@ class mjh extends eqLogic {
   public static function dependancy_info() {
     $return = array();
     $return['log'] = 'mjh_dep';
-    $return['state'] = 'nok';
+    $return['state'] = 'ok';
 
     return $return;
   }
@@ -35,7 +35,7 @@ class mjh extends eqLogic {
 
   public static function deamon_info() {
     $return = array();
-    $return['log'] = 'mjh';
+    $return['log'] = 'mjh_deamon';
     $return['launchable'] = 'ok';
 
     $pid = trim(shell_exec('ps ax | grep "mjh/node/deamon.js" | grep -v "grep" | wc -l'));
@@ -48,62 +48,20 @@ class mjh extends eqLogic {
     return $return;
   }
 
-    /*     * *********************Méthodes d'instance************************* */
+  public static function deamon_start($_debug = false) {
+    log::add('mjh', 'info', 'Deamon start');
+  }
 
-    public function preInsert() {
+  public static function deamon_stop() {
+    log::add('mjh', 'info', 'Deamon stop');
+  }
 
-    }
-
-    public function postInsert() {
-
-    }
-
-    public function preSave() {
-
-    }
-
-    public function postSave() {
-
-    }
-
-    public function preUpdate() {
-
-    }
-
-    public function postUpdate() {
-
-    }
-
-    public function preRemove() {
-
-    }
-
-    public function postRemove() {
-
-    }
-
-    /*     * **********************Getteur Setteur*************************** */
 }
 
 class mjhCmd extends cmd {
-    /*     * *************************Attributs****************************** */
-
-
-    /*     * ***********************Methode static*************************** */
-
-
-    /*     * *********************Methode d'instance************************* */
-
-    /*
-     * Non obligatoire permet de demander de ne pas supprimer les commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
-      public function dontRemoveCmd() {
-      return true;
-      }
-     */
 
     public function execute($_options = array()) {
 
     }
 
-    /*     * **********************Getteur Setteur*************************** */
 }
