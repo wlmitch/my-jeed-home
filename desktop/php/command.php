f241f1a273d8639be970bb8f9edc3cff06ce7dc8
WLMitch <wlmitch@gmail.com>
2018-07-31 01:35:46 +0200

    remove groups

--- desktop/php/command.php
+++ desktop/php/command.php
@@ -6,32 +6,34 @@
     </td>
     <td>
       <div class="form-group">
-        <input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom}}">
-      </div>
-      <div class="form-group">
+        <div>
+          <input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom}}">
+        </div>
+        <div>
           <label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked=""> {{Afficher}}</label>
           <label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label>
+        </div>
       </div>
     </td>
     <td>
-      <div class="form-group type" type="[[type]]">[[&types]]</div>
-      <div class="form-group subType" subType="[[subType]]"></div>
+      <div class="type" type="[[type]]">[[&types]]</div>
+      <div class="subType" subType="[[subType]]"></div>
     </td>
     <td>
       <!-- Commande -->
-      <div class="form-group">
+      <div>
         <input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="value" placeholder="{{Valeur}}"/>
       </div>
       <!-- Info (pour le retour d'état) -->
-      <div class="form-group">
+      <div>
         <select class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="updateCmdId" title="Information à mettre à jour">
       </div>
     </td>
     <td>
-      <div class="form-group">
+      <div>
           <input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="{{Unité}}">
       </div>
-      <div class="form-group input-group">
+      <div class="input-group">
         <input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width : 50%;" />
         <input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width : 50%;" />
       </div>
