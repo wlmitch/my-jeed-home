2f64d6051bf4575cfbfe81798bf30006b1b1952d
WLMitch <wlmitch@gmail.com>
2018-07-30 23:11:55 +0200

    replace ${ } by [[]]

--- desktop/php/command.php
+++ desktop/php/command.php
@@ -1,6 +1,6 @@
 
 <script id="command-template" type="text/template">
-  <tr class="cmd" data-cmd_id="${id}">
+  <tr class="cmd" data-cmd_id="[[id]]">
     <td>
       <span class="cmdAttr" data-l1key="id"></span>
     </td>
@@ -9,8 +9,8 @@
       <input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">
     </td>
     <td>
-      <span class="type" type="${type}">${types}</span>
-      <span class="subType" subType="${subType}">${types}</span>
+      <span class="type" type="[[type]]">[[$types]]</span>
+      <span class="subType" subType="[[subType]]"></span>
     </td>
     <td>
       Valeur
@@ -21,12 +21,14 @@
       <input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width : 40%;display : inline-block;" />
       <input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width : 40%;display : inline-block;" />
     </td>
-    <td class="btn-group">
-      ${#id}
-      <a class="btn btn-default btn-xs cmdAction" data-action="configure" data-toggle="tooltip" title="Configure"><i class="fa fa-cogs"></i></a>
-      <a class="btn btn-default btn-xs cmdAction" data-action="test" data-toggle="tooltip" title="Test"><i class="fa fa-rss"></i></a>
-      ${/id}
-      <a class="btn btn-default btn-xs cmdAction" data-action="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-minus-circle"></i></a>
+    <td>
+      <div class="btn-group">
+        [[#id]]
+        <a class="btn btn-default btn-xs cmdAction" data-action="configure" data-toggle="tooltip" title="Configure"><i class="fa fa-cogs"></i></a>
+        <a class="btn btn-default btn-xs cmdAction" data-action="test" data-toggle="tooltip" title="Test"><i class="fa fa-rss"></i></a>
+        [[/id]]
+        <a class="btn btn-default btn-xs cmdAction" data-action="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-minus-circle"></i></a>
+      </div>
     </td>
   </tr>
 </script>
