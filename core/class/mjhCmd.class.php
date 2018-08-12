d02dbb34a2bb350908cc225675410f1f935595ac
Alexandre JANNOTTA <wlmitch@gmail.com>
2018-08-12 16:23:37 +0200

    remove info execute

--- core/class/mjhCmd.class.php
+++ core/class/mjhCmd.class.php
@@ -3,8 +3,10 @@
 class mjhCmd extends cmd {
 
 	public function execute($_options = array()) {
-		log::add('mjh', 'debug', 'Command execute : ' . $this->getId());
-		mjh::sendCommand($this->getConfiguration()['command']);
+		if ($this->getType() == 'action') {
+			log::add('mjh', 'debug', 'Command execute : ' . $this->getId());
+			mjh::sendCommand($this->getConfiguration()['command']);
+		}
 	}
 
 }
