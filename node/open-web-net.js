ae79a1a0e4dadbb460835862d864e0e2bff5ec6f
Alexandre JANNOTTA <wlmitch@gmail.com>
2018-08-16 23:48:43 +0200

    Cleanup + ignore 13

--- node/open-web-net.js
+++ node/open-web-net.js
@@ -9,50 +9,37 @@ const LOGGER = require('simple-node-logger').createSimpleLogger({
 });
 LOGGER.setLevel(process.env.LOG_LEVEL);
 
-// #############
-// # Constants #
-// #############
-
-const MESSAGE_BEGIN = '*'
-const MESSAGE_END = '##'
-
-const MESSAGE_ACK = '*#*1##'
-const MESSAGE_NACK = '*#*0##'
-//const MESSAGE_STATUS_REQUEST = '*#<who>*<where>##'
-const MESSAGE_STATUS = '*<who>*<what>*<where>##'
-//const MESSAGE_DIMENSION_REQUEST = '*#<who>*<where>*<dimension>##'
-//const MESSAGE_DIMENSION_WRITING = '*#<who>*<where>*#<dimension>*<val_1>*...*<val_n>##'
-const MESSAGE_DIMENSION = '*#<who>*<where>*<dimension>*<val_1>*...*<val_n>##'
-
-const TAG_WHO = ''
-//  0 - Scenario
-//  1 - Light
-//  2 - Automation
-//  4 - Thermoregulation
-//  5 - Alarm
-//  7 - Video
-// 13 - Gateway
-// 15 - CEN
-// 17 - Scenes
-// 18 - Energy
-// 22 - Sound
-// 24 - Light management
-// 25 - CEN+ / Dry contact
-const TAG_WHAT = '<what>#<par_1>#...#<par_n>'
-const TAG_WHERE = '<where>#<par_1>#...#<par_n>'
-
 // ##############
 // # Translator #
 // ##############
 
 // Documentation : https://www.myopen-legrandgroup.com/developers/
+// ACK : *#*1##
+// NACK : *#*0##
+// STATUS_REQUEST : *#<who>*<where>#<par_1>#...#<par_n>##
+// STATUS_RESPONSE : *<who>*<what>#<par_1>#...#<par_n>*<where>#<par_1>#...#<par_n>##
+// DIMENSION_REQUEST : *#<who>*<where>#<par_1>#...#<par_n>*<dimension>##
+// DIMENSION_WRITING : *#<who>*<where>#<par_1>#...#<par_n>*#<dimension>*<val_1>*...*<val_n>##
+// DIMENSION_RESPONSE : *#<who>*<where>#<par_1>#...#<par_n>*<dimension>*<val_1>*...*<val_n>##
+
 class Translator {
 
 	constructor() {
 		this.translators = {
-			2: this.automation,
-			18: this.ignore,
-			1022: this.ignore
+			//  0 - Scenario
+			//  1 - Light
+			2: this.automation, // Automation
+			13: this.ignore, // Gateway
+			//  4 - Thermoregulation
+			//  5 - Alarm
+			//  7 - Video
+			// 15 - CEN
+			// 17 - Scenes
+			18: this.ignore, // Energy
+			// 22 - Sound
+			// 24 - Light management
+			// 25 - CEN+ / Dry contact
+			1022: this.ignore // ???
 		}
 	}
 
